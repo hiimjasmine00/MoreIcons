2c0c6082c460c921c4a21752730234de6bc5a761
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-12-23 07:34:54 -0500

    Death effect settings editor

--- src/classes/popup/info/SpecialSettingsPopup.cpp
+++ src/classes/popup/info/SpecialSettingsPopup.cpp
@@ -13,7 +13,11 @@ using namespace geode::prelude;
 SpecialSettingsPopup* SpecialSettingsPopup::create(IconInfo* info) {
     auto ret = new SpecialSettingsPopup();
     auto type = info->getType();
-    if (ret->initAnchored(400.0f, type == IconType::ShipFire ? 200.0f : 150.0f, info)) {
+    if (ret->initAnchored(
+        type == IconType::DeathEffect ? 500.0f : 400.0f,
+        type == IconType::DeathEffect ? 280.0f : type == IconType::ShipFire ? 200.0f : 150.0f,
+        info
+    )) {
         ret->autorelease();
         return ret;
     }
@@ -34,20 +38,44 @@ bool SpecialSettingsPopup::setup(IconInfo* info) {
 
     m_settings = info->getSpecialInfo();
 
-    if (type == IconType::Special) {
-        addControl("fade", "Fade Time:", { 100.0f, 87.5f }, 0.0f, 2.0f, 0.3f, 2);
-        addControl("stroke", "Stroke Width:", { 300.0f, 87.5f }, 0.0f, 20.0f, 14.0f, 1);
-        addToggle("blend", "Additive\nBlending", { 60.0f, 60.0f }, false);
-        addToggle("tint", "Tint\nColor", { 170.0f, 60.0f }, false);
-        addToggle("show", "Always\nShow", { 280.0f, 60.0f }, true);
+    if (type == IconType::DeathEffect) {
+        addControl("scale", "Scale:", { 65.0f, 225.0f }, 0.7f, 0.0f, 2.0f, 1.0f, 2);
+        addControl("scale-variance", "+-:", { 565.0f / 3.0f, 225.0f }, 0.7f, 0.0f, 1.0f, 0.0f, 2);
+        addControl("rotation", "Rotation:", { 935.0f / 3.0f, 225.0f }, 0.7f, 0.0f, 360.0f, 0.0f, 0);
+        addControl("rotation-variance", "+-:", { 435.0f, 225.0f }, 0.7f, 0.0f, 360.0f, 0.0f, 0);
+        addToggle("blend", "Additive\nBlending", { 70.0f, 200.0f }, 1.0f, false);
+        addControl("frame-delay", "Frame Delay:", { 250.0f, 192.0f }, 0.7f, 0.0f, 0.1f, 0.0f, 3);
+        addControl("frame-delay-variance", "+-:", { 400.0f, 192.0f }, 0.7f, 0.0f, 0.1f, 0.0f, 3);
+        addToggle("fade", "Fade\nOut", { 70.0f, 168.0f }, 1.0f, true);
+        addControl("fade-delay-multiplier", "Fade Delay:", { 250.0f, 160.0f }, 0.7f, 0.0f, 20.0f, 6.0f, 1);
+        addControl("fade-time-multiplier", "Fade Time:", { 400.0f, 160.0f }, 0.7f, 0.0f, 20.0f, 6.0f, 1);
+        addLabel("circle-label", "Circle Effect", { 250.0f, 140.0f });
+        addControl("circle-start-radius", "Start Radius:", { 65.0f, 105.0f }, 0.7f, 0.0f, 20.0f, 10.0f, 2);
+        addControl("circle-end-radius", "End Radius:", { 565.0f / 3.0f, 105.0f }, 0.7f, 0.0f, 200.0f, 110.0f, 1);
+        addToggle("circle-use-scale", "Use\nScale", { 262.5f, 113.0f }, 0.8f, false);
+        addControl("circle-duration", "Duration:", { 373.5f, 105.0f }, 0.7f, 0.0f, 20.0f, 0.6f, 2);
+        addToggle("circle-use-delay", "Use\nDelay", { 446.0f, 113.0f }, 0.8f, false);
+        addLabel("outline-label", "Outline Effect", { 250.0f, 85.0f });
+        addControl("outline-start-radius", "Start Radius:", { 65.0f, 50.0f }, 0.7f, 0.0f, 20.0f, 10.0f, 2);
+        addControl("outline-end-radius", "End Radius:", { 565.0f / 3.0f, 50.0f }, 0.7f, 0.0f, 200.0f, 115.0f, 1);
+        addToggle("outline-use-scale", "Use\nScale", { 262.5f, 58.0f }, 0.8f, false);
+        addControl("outline-duration", "Duration:", { 373.5f, 50.0f }, 0.7f, 0.0f, 20.0f, 0.4f, 2);
+        addToggle("outline-use-delay", "Use\nDelay", { 446.0f, 58.0f }, 0.8f, false);
+    }
+    else if (type == IconType::Special) {
+        addControl("fade", "Fade Time:", { 100.0f, 87.5f }, 1.0f, 0.0f, 2.0f, 0.3f, 2);
+        addControl("stroke", "Stroke Width:", { 300.0f, 87.5f }, 1.0f, 0.0f, 20.0f, 14.0f, 1);
+        addToggle("blend", "Additive\nBlending", { 60.0f, 60.0f }, 1.0f, true);
+        addToggle("tint", "Tint\nColor", { 170.0f, 60.0f }, 1.0f, false);
+        addToggle("show", "Always\nShow", { 280.0f, 60.0f }, 1.0f, true);
     }
     else if (type == IconType::ShipFire) {
-        addControl("fade", "Fade Time:", { 100.0f, 135.0f }, 0.0f, 2.0f, 0.1f, 2);
-        addControl("stroke", "Stroke Width:", { 300.0f, 135.0f }, 0.0f, 40.0f, 20.0f, 1);
-        addControl("x", "Offset X:", { 100.0f, 92.5f }, -20.0f, 20.0f, 0.0f, 1);
-        addControl("y", "Offset Y:", { 300.0f, 92.5f }, -20.0f, 20.0f, 0.0f, 1);
-        addControl("interval", "Fire Interval:", { 100.0f, 50.0f }, 0.0f, 1.0f, 0.5f, 2);
-        addToggle("blend", "Additive\nBlending", { 280.0f, 60.0f }, true);
+        addControl("fade", "Fade Time:", { 100.0f, 135.0f }, 1.0f, 0.0f, 2.0f, 0.1f, 2);
+        addControl("stroke", "Stroke Width:", { 300.0f, 135.0f }, 1.0f, 0.0f, 40.0f, 20.0f, 1);
+        addControl("x", "Offset X:", { 100.0f, 92.5f }, 1.0f, -20.0f, 20.0f, 0.0f, 1);
+        addControl("y", "Offset Y:", { 300.0f, 92.5f }, 1.0f, -20.0f, 20.0f, 0.0f, 1);
+        addControl("interval", "Fire Interval:", { 100.0f, 50.0f }, 1.0f, 0.0f, 1.0f, 0.5f, 2);
+        addToggle("blend", "Additive\nBlending", { 280.0f, 60.0f }, 1.0f, true);
     }
 
     auto saveButton = CCMenuItemExt::createSpriteExtra(ButtonSprite::create("Save", 0.8f), [this, info](auto) {
@@ -59,7 +87,7 @@ bool SpecialSettingsPopup::setup(IconInfo* info) {
             onClose(nullptr);
         }
     });
-    saveButton->setPosition({ 200.0f, 25.0f });
+    saveButton->setPosition({ m_size.width / 2.0f, 25.0f });
     saveButton->setID("save-button");
     m_buttonMenu->addChild(saveButton);
 
@@ -69,26 +97,33 @@ bool SpecialSettingsPopup::setup(IconInfo* info) {
 }
 
 void SpecialSettingsPopup::addControl(
-    std::string_view id, const char* text, const CCPoint& position, float min, float max, float def, int decimals
+    std::string_view id, const char* text, const CCPoint& position, float scale, float min, float max, float def, int decimals
 ) {
     auto& value = m_settings[id];
     if (!value.isNumber()) value = def;
     auto initial = value.as<float>().unwrapOr(def);
     auto factor = max - min;
 
+    auto positioner = CCNode::create();
+    positioner->setPosition(position);
+    positioner->setScale(scale);
+    positioner->setAnchorPoint({ 0.5f, 0.5f });
+    positioner->setID(fmt::format("{}-positioner", id));
+    m_mainLayer->addChild(positioner);
+
     auto slider = Slider::create(nullptr, nullptr, 0.8f);
-    slider->setPosition(position);
+    slider->setPosition({ 0.0f, 0.0f });
     slider->setValue((initial - min) / factor);
     slider->setID(fmt::format("{}-slider", id));
-    m_mainLayer->addChild(slider);
+    positioner->addChild(slider);
 
     auto container = CCNode::create();
-    container->setPosition(position + CCPoint { 0.0f, 22.5f });
+    container->setPosition({ 0.0f, 22.5f });
     container->setContentSize({ 170.0f, 30.0f });
     container->setAnchorPoint({ 0.5f, 0.5f });
     container->setLayout(RowLayout::create()->setAutoScale(false));
     container->setID(fmt::format("{}-container", id));
-    m_mainLayer->addChild(container);
+    positioner->addChild(container);
 
     auto label = CCLabelBMFont::create(text, "goldFont.fnt");
     label->setScale(0.7f);
@@ -124,22 +159,39 @@ void SpecialSettingsPopup::addControl(
     });
 }
 
-void SpecialSettingsPopup::addToggle(std::string_view id, const char* label, const cocos2d::CCPoint& position, bool def) {
+void SpecialSettingsPopup::addToggle(std::string_view id, const char* label, const cocos2d::CCPoint& position, float scale, bool def) {
     auto& value = m_settings[id];
     if (!value.isBool()) value = def;
+
+    auto positioner = CCMenu::create();
+    positioner->setPosition(position);
+    positioner->setScale(scale);
+    positioner->setContentSize({ 0.0f, 0.0f });
+    positioner->setAnchorPoint({ 0.5f, 0.5f });
+    positioner->ignoreAnchorPointForPosition(false);
+    positioner->setID(fmt::format("{}-positioner", id));
+    m_mainLayer->addChild(positioner);
 
     auto toggle = CCMenuItemExt::createTogglerWithStandardSprites(0.8f, [this, def, &value](auto) {
         value = !value.asBool().unwrapOr(def);
     });
-    toggle->setPosition(position);
+    toggle->setPosition({ 0.0f, 0.0f });
     toggle->toggle(value.asBool().unwrapOr(def));
     toggle->setID(fmt::format("{}-toggle", id));
-    m_buttonMenu->addChild(toggle);
+    positioner->addChild(toggle);
 
     auto toggleLabel = CCLabelBMFont::create(label, "bigFont.fnt");
-    toggleLabel->setPosition(position + CCPoint { 20.0f, 0.0f });
+    toggleLabel->setPosition({ 20.0f, 0.0f });
     toggleLabel->setScale(0.4f);
     toggleLabel->setAnchorPoint({ 0.0f, 0.5f });
     toggleLabel->setID(fmt::format("{}-label", id));
-    m_mainLayer->addChild(toggleLabel);
+    positioner->addChild(toggleLabel);
+}
+
+void SpecialSettingsPopup::addLabel(std::string&& id, const char* text, const cocos2d::CCPoint& position) {
+    auto label = CCLabelBMFont::create(text, "goldFont.fnt");
+    label->setPosition(position);
+    label->setScale(0.7f);
+    label->setID(std::move(id));
+    m_mainLayer->addChild(label);
 }
--- src/classes/popup/info/SpecialSettingsPopup.hpp
+++ src/classes/popup/info/SpecialSettingsPopup.hpp
@@ -7,8 +7,11 @@ protected:
     matjson::Value m_settings;
 
     bool setup(IconInfo* info) override;
-    void addControl(std::string_view id, const char* text, const cocos2d::CCPoint& position, float min, float max, float def, int decimals);
-    void addToggle(std::string_view id, const char* text, const cocos2d::CCPoint& position, bool def);
+    void addControl(
+        std::string_view id, const char* text, const cocos2d::CCPoint& position, float scale, float min, float max, float def, int decimals
+    );
+    void addToggle(std::string_view id, const char* text, const cocos2d::CCPoint& position, float scale, bool def);
+    void addLabel(std::string&& id, const char* text, const cocos2d::CCPoint& position);
 public:
     static SpecialSettingsPopup* create(IconInfo* info);
 };
