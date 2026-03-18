f957c54cb154f99a0247a91780b5df2dddbffd16
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-03-17 22:35:50 -0400

    Fix Globed preloading

--- src/hooks/GameManager.cpp
+++ src/hooks/GameManager.cpp
@@ -1,12 +1,34 @@
+#include "../utils/Filesystem.hpp"
 #include "../utils/Get.hpp"
 #include "../utils/Icons.hpp"
 #include "../utils/Log.hpp"
+#include <Geode/loader/Dirs.hpp>
 #include <Geode/modify/GameManager.hpp>
 #include <jasmine/hook.hpp>
 #include <jasmine/setting.hpp>
 #include <MoreIcons.hpp>
 
 using namespace geode::prelude;
+
+std::string addSuffix(std::string str, std::string_view suffix) {
+    if (!str.contains(suffix)) {
+        auto dotPos = str.rfind('.');
+        auto dashPos = str.rfind('-');
+        if (dotPos != std::string::npos && (dashPos == std::string::npos || dotPos > dashPos)) {
+            str.insert(dotPos, suffix);
+        }
+    }
+    return std::move(str);
+}
+
+bool isFileExist(const gd::string& path) {
+    #ifdef GEODE_IS_WINDOWS
+    auto attrs = GetFileAttributesA(path);
+    return (attrs != INVALID_FILE_ATTRIBUTES && (attrs & FILE_ATTRIBUTE_DIRECTORY) == 0);
+    #else
+    return Get::fileUtils->isFileExist(path);
+    #endif
+}
 
 class $modify(MIGameManager, GameManager) {
     static void onModify(ModifyBase<ModifyDerive<MIGameManager, GameManager>>& self) {
@@ -47,7 +69,39 @@ class $modify(MIGameManager, GameManager) {
     gd::string sheetNameForIcon(int id, int type) {
         auto ret = GameManager::sheetNameForIcon(id, type);
         if (ret.empty() || id < 1) return ret;
-        return Get::fileUtils->fullPathForFilename(ret.c_str(), false);
+        auto factor = Get::director->getContentScaleFactor();
+        #ifdef GEODE_IS_MOBILE
+        if (factor >= 4.0f) {
+            auto uhdPath = Icons::vanillaTexturePath(fmt::format("{}.png", ret), false);
+            auto uhdSuffix = addSuffix(std::move(Filesystem::getPathString(uhdPath)), "-uhd");
+            if (isFileExist(uhdSuffix)) {
+                uhdSuffix.resize(uhdSuffix.size() - 4);
+                return uhdSuffix;
+            }
+        }
+        #endif
+        auto path = string::pathToString(dirs::getResourcesDir() / fmt::format(L("{}.png"), Filesystem::strWide(ret)));
+        #ifndef GEODE_IS_MOBILE
+        if (factor >= 4.0f) {
+            auto uhdSuffix = addSuffix(path, "-uhd");
+            if (isFileExist(uhdSuffix)) {
+                uhdSuffix.resize(uhdSuffix.size() - 4);
+                return uhdSuffix;
+            }
+        }
+        #endif
+        if (factor >= 2.0f) {
+            auto hdSuffix = addSuffix(path, "-hd");
+            if (isFileExist(hdSuffix)) {
+                hdSuffix.resize(hdSuffix.size() - 4);
+                return hdSuffix;
+            }
+        }
+        if (isFileExist(path)) {
+            path.resize(path.size() - 4);
+            return path;
+        }
+        return ret;
     }
 
     CCTexture2D* loadIcon(int id, int type, int requestID) {
