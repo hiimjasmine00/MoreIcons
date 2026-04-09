fcf3f390b7c26095fa3f42ed56a2d554a12900b5
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-04-09 01:32:26 -0400

    https://youtu.be/4-GNV9DEF98?t=33

--- src/classes/popup/edit/ImageRenderer.cpp
+++ src/classes/popup/edit/ImageRenderer.cpp
@@ -75,40 +75,48 @@ Result<std::vector<uint8_t>> ImageRenderer::getImage(CCTexture2D* texture) {
     auto height = texture->getPixelsHigh();
 
     std::vector<uint8_t> data(width * height * 4);
-    auto oldTexture = 0;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &oldTexture);
-    glBindTexture(GL_TEXTURE_2D, texture->getName());
-    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data.data());
-    glBindTexture(GL_TEXTURE_2D, oldTexture);
+
+    auto oldFBO = 0;
+    auto fbo = 0u;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &oldFBO);
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture->getName(), 0);
+    glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data.data());
+    glBindFramebuffer(GL_FRAMEBUFFER, oldFBO);
+    glDeleteFramebuffers(1, &fbo);
 
     return texpack::toPNG(data, width, height);
 }
 
 texpack::Image ImageRenderer::getImage(CCSpriteFrame* frame) {
     auto texture = frame->getTexture();
-    auto textureWidth = texture->getPixelsWide();
-    auto textureHeight = texture->getPixelsHigh();
 
-    std::vector<uint8_t> textureData(textureWidth * textureHeight * 4);
-    auto oldTexture = 0;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &oldTexture);
-    glBindTexture(GL_TEXTURE_2D, texture->getName());
-    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData.data());
-    glBindTexture(GL_TEXTURE_2D, oldTexture);
+    auto& frameRect = frame->m_obRectInPixels;
+    uint32_t w = frameRect.size.width;
+    uint32_t h = frameRect.size.height;
+
+    auto rotated = frame->m_bRotated;
+
+    std::vector<uint8_t> frameData(w * h * 4);
+
+    auto oldFBO = 0;
+    auto fbo = 0u;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &oldFBO);
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture->getName(), 0);
+    glReadPixels(frameRect.origin.x, frameRect.origin.y, rotated ? h : w, rotated ? w : h, GL_RGBA, GL_UNSIGNED_BYTE, frameData.data());
+    glBindFramebuffer(GL_FRAMEBUFFER, oldFBO);
+    glDeleteFramebuffers(1, &fbo);
 
     auto& frameSize = frame->m_obOriginalSizeInPixels;
     uint32_t width = frameSize.width;
     uint32_t height = frameSize.height;
 
-    if (width == textureWidth && height == textureHeight) {
-        return { std::move(textureData), width, height };
+    if (width == w && height == h) {
+        return { std::move(frameData), width, height };
     }
-
-    auto& frameRect = frame->m_obRectInPixels;
-    uint32_t baseX = frameRect.origin.x;
-    uint32_t baseY = frameRect.origin.y;
-    uint32_t w = frameRect.size.width;
-    uint32_t h = frameRect.size.height;
 
     auto& frameOffset = frame->m_obOffsetInPixels;
     int offsetX = frameOffset.x;
@@ -124,27 +132,27 @@ texpack::Image ImageRenderer::getImage(CCSpriteFrame* frame) {
     left /= 2;
     left += offsetX;
 
-    if (frame->m_bRotated) {
-        for (uint32_t x = h - 1; x != UINT_MAX; x--) {
-            for (uint32_t y = 0; y < w; y++) {
-                auto src = ((baseY + y) * textureWidth + baseX + x) * 4;
+    if (rotated) {
+        for (uint32_t y = 0; y < w; y++) {
+            for (uint32_t x = 0; x < h; x++) {
+                auto src = (y * h + x) * 4;
                 auto dst = ((top + h - x - 1) * width + left + y) * 4;
-                data[dst] = textureData[src];
-                data[dst + 1] = textureData[src + 1];
-                data[dst + 2] = textureData[src + 2];
-                data[dst + 3] = textureData[src + 3];
+                data[dst] = frameData[src];
+                data[dst + 1] = frameData[src + 1];
+                data[dst + 2] = frameData[src + 2];
+                data[dst + 3] = frameData[src + 3];
             }
         }
     }
     else {
         for (uint32_t y = 0; y < h; y++) {
             for (uint32_t x = 0; x < w; x++) {
-                auto src = ((baseY + y) * textureWidth + baseX + x) * 4;
+                auto src = (y * w + x) * 4;
                 auto dst = ((top + y) * width + left + x) * 4;
-                data[dst] = textureData[src];
-                data[dst + 1] = textureData[src + 1];
-                data[dst + 2] = textureData[src + 2];
-                data[dst + 3] = textureData[src + 3];
+                data[dst] = frameData[src];
+                data[dst + 1] = frameData[src + 1];
+                data[dst + 2] = frameData[src + 2];
+                data[dst + 3] = frameData[src + 3];
             }
         }
     }
